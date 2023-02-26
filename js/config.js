d95a6f97dfc20866744ad30ff4cc0f8ed1114458
marsvoyager <marsvoyager@icloud.com>
2023-02-26 16:00:08 +0100

    Integración rápida de gestión de usuarios en pruebas. Mezcla con el código principal del juego.

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-3_N2Ilvo2j7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7tspfjf14p46rnnj8243c84s7r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-3' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
